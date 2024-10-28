7637f0c67ca5d4a2e36e9dc158d5c73d1555a314
GS <91503751+gaasol13@users.noreply.github.com>
2024-10-28 17:45:21 +0000

    Testing connection with DB
    I included the text of EntityManager to test the connectivity with the root database, this was succesful!

--- Java/src/main/java/com/poortoys/examples/AppMain.java
+++ Java/src/main/java/com/poortoys/examples/AppMain.java
@@ -1,9 +1,25 @@
 package com.poortoys.examples;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+//import com.example.entities.Genre;
+import java.util.List;
+
 public class AppMain {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ticketingsystem");
+        EntityManager em = emf.createEntityManager();
+        
+        try {
+        	List<Genre> genres = em.createQuery("SELECT g FROM Genre g", Genre.class).getResultList();
+        	genres.forEach(genre -> System.out.println(genre.getGenreName()));
+        }finally {
+        	em.close();
+        	emf.close();
+        }
+        
     }
 
 }
