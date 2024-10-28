c890c9533d7ffdada1ad281be8501709f777c0fa
GS <91503751+gaasol13@users.noreply.github.com>
2024-10-28 20:48:08 +0000

    Update AppMain.java

--- Java/src/main/java/com/poortoys/examples/AppMain.java
+++ Java/src/main/java/com/poortoys/examples/AppMain.java
@@ -3,22 +3,35 @@ package com.poortoys.examples;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
+import com.poortoys.examples.entities.Genre;
+
 //import com.example.entities.Genre;
 import java.util.List;
 
 public class AppMain {
 
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ticketingsystem");
-        EntityManager em = emf.createEntityManager();
-        
-        try {
-        	List<Genre> genres = em.createQuery("SELECT g FROM Genre g", Genre.class).getResultList();
-        	genres.forEach(genre -> System.out.println(genre.getGenreName()));
-        }finally {
-        	em.close();
-        	emf.close();
-        }
+
+    	
+		
+		  EntityManagerFactory emf = Persistence.createEntityManagerFactory("ticketingsystem"); 
+		  EntityManager em =  emf.createEntityManager();
+		  
+		  try { 
+			  List<Genre> genres = em.createQuery("SELECT g FROM Genre g",Genre.class).getResultList(); 
+			  
+			  genres.forEach(genre -> System.out.println(genre.getGenreName())); 
+			  
+			  } finally { em.close();
+			  
+			  emf.close(); 
+		  }
+		 
+		   
+    	
+    	
+    	
         
     }
 
