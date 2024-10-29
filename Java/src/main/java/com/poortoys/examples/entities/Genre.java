eef919a201443c0297237394e8be519c6a24a658
GS <91503751+gaasol13@users.noreply.github.com>
2024-10-29 23:17:59 +0000

    Update Genre.java

--- Java/src/main/java/com/poortoys/examples/entities/Genre.java
+++ Java/src/main/java/com/poortoys/examples/entities/Genre.java
@@ -6,20 +6,21 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 
 @Entity //Specifies that the class is an entity and is mapped to a database table
-@Table(name = "genres") //Maps the entity to the tickets table
+@Table(name = "genres",uniqueConstraints = {@UniqueConstraint(columnNames = {"genre_name"})}) //Maps the entity to the tickets table
 public class Genre {
 	
 	//Column >ID 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) //Denotes the primary key and its generation strategy
 	@Column(name = "genre_id")//Maps class fields to table columns
-	private int genreId;
+	private Integer genreId;
 	
 	//
-	@Column(name = "genre_name", nullable = false, length = 35)
+	@Column(name = "genre_name", nullable = false, unique = true, length = 35)
 	private String genreName;
 
 	//Constructors
