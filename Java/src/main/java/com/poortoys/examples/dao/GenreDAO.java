3f19004e56a1a2b208cf2df8add6633aa0e5773f
GS <91503751+gaasol13@users.noreply.github.com>
2024-10-29 19:07:21 +0000

    Update GenreDAO.java

--- Java/src/main/java/com/poortoys/examples/dao/GenreDAO.java
+++ Java/src/main/java/com/poortoys/examples/dao/GenreDAO.java
@@ -1,5 +1,11 @@
 package com.poortoys.examples.dao;
 
+import javax.persistence.EntityManager;
+
 public class GenreDAO {
+
+	public GenreDAO(EntityManager em) {
+		// TODO Auto-generated constructor stub
+	}
 
 }
