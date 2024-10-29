b852b8a261e5b99b476e691f57fa269197f4c99d
GS <91503751+gaasol13@users.noreply.github.com>
2024-10-29 23:17:55 +0000

    Update VenueDAO.java

--- Java/src/main/java/com/poortoys/examples/dao/VenueDAO.java
+++ Java/src/main/java/com/poortoys/examples/dao/VenueDAO.java
@@ -2,10 +2,22 @@ package com.poortoys.examples.dao;
 
 import javax.persistence.EntityManager;
 
+import com.poortoys.examples.entities.Venue;
+
 public class VenueDAO {
 
 	public VenueDAO(EntityManager em) {
 		// TODO Auto-generated constructor stub
+	}
+
+	public Venue findByName(String venueName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void create(Venue venue) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
