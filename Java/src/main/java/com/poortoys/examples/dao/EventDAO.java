365899615c47149cddaee4dcdff0698bcb30d7b2
GS <91503751+gaasol13@users.noreply.github.com>
2024-10-29 23:17:34 +0000

    Update EventDAO.java

--- Java/src/main/java/com/poortoys/examples/dao/EventDAO.java
+++ Java/src/main/java/com/poortoys/examples/dao/EventDAO.java
@@ -2,10 +2,22 @@ package com.poortoys.examples.dao;
 
 import javax.persistence.EntityManager;
 
+import com.poortoys.examples.entities.Event;
+
 public class EventDAO {
 
 	public EventDAO(EntityManager em) {
 		// TODO Auto-generated constructor stub
+	}
+
+	public Event findByName(String eventName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void create(Event event) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
