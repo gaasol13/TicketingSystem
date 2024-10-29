daf9d4ca82d98926cc3dd32d408ad547ad83e2dd
GS <91503751+gaasol13@users.noreply.github.com>
2024-10-29 23:17:51 +0000

    Update PerformerDAO.java

--- Java/src/main/java/com/poortoys/examples/dao/PerformerDAO.java
+++ Java/src/main/java/com/poortoys/examples/dao/PerformerDAO.java
@@ -2,10 +2,22 @@ package com.poortoys.examples.dao;
 
 import javax.persistence.EntityManager;
 
+import com.poortoys.examples.entities.Performer;
+
 public class PerformerDAO {
 
 	public PerformerDAO(EntityManager em) {
 		// TODO Auto-generated constructor stub
+	}
+
+	public void create(Performer performer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Performer findByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
