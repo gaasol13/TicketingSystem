82fe2c646307261bea7c0f483a2eda760d7d51c4
GS <91503751+gaasol13@users.noreply.github.com>
2024-11-09 23:41:47 +0000

    Update Genre.java

--- MongoDB/ticketSystemMongoDB/src/main/java/com/ticketing/system/entities/Genre.java
+++ MongoDB/ticketSystemMongoDB/src/main/java/com/ticketing/system/entities/Genre.java
@@ -3,49 +3,52 @@ package com.ticketing.system.entities;
 
 import org.bson.types.ObjectId;
 
+import dev.morphia.annotations.Entity;
+import dev.morphia.annotations.Id;
+import dev.morphia.annotations.Indexed;
+import dev.morphia.annotations.Property;
+
 /**
  * Represents a music genre in the ticketing system.
  */
+
+@Entity("genres")
 public class Genre {
+	
+	@Id 
     private ObjectId id; // MongoDB's unique identifier
-    private String name; // Name of the genre (e.g., Rock, Jazz)
+	
+	@Indexed(options = @dev.morphia.annotations.IndexOptions(unique = true))//Creates a unique index on genreName
+	@Property("genreName") // Maps this field to the "genreNAme field in mongoDB
+    private String genreName; // Name of the genre (e.g., Rock, Jazz)
 
     // Default constructor
     public Genre() {
     }
 
     // Parameterized constructor
-    public Genre(String name) {
-        this.name = name;
+    public Genre(String genreName) {
+        this.genreName = genreName;
     }
 
     // Getters and Setters
 
     /**
      * Gets the unique identifier of the genre.
-     * 
-     * @return ObjectId of the genre.
      */
     public ObjectId getId() {
         return id;
     }
 
     /**
-     * Sets the unique identifier of the genre.
-     * 
-     * @param id ObjectId to set.
-     */
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    /**
      * Gets the name of the genre.
-     * 
-     * @return Name of the genre.
      */
-    public String getName() {
-        return name;
+    public String getGenreName() {
+        return genreName;
+    }
+    
+    public void setGenreName(String genreName) { // Renamed from setName to setGenreName
+        this.genreName = genreName;
     }
 
     /**
@@ -54,13 +57,13 @@ public class Genre {
      * @param name Name to set.
      */
     public void setName(String name) {
-        this.name = name;
+        this.genreName = name;
     }
 
     // toString method for debugging purposes
 
     @Override
     public String toString() {
-        return "Genre{id=" + id + ", name='" + name + "'}";
+        return "Genre{id=" + id + ", genreName='" + genreName + "'}";
     }
 }
