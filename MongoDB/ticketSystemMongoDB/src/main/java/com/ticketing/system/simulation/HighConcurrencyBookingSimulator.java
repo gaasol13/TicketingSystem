952250b0bab4e2ab90c99ea3080e0b5decb0e2ee
GS <91503751+gaasol13@users.noreply.github.com>
2024-11-13 20:07:25 +0000

    tests

---
+++ MongoDB/ticketSystemMongoDB/src/main/java/com/ticketing/system/simulation/HighConcurrencyBookingSimulator.java
@@ -0,0 +1,10 @@
+package com.ticketing.system.simulation;
+
+public class HighConcurrencyBookingSimulator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
